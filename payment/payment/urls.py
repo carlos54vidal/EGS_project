172921d5d35ecfd9c468d40dfb325ee90950397f
carlos54vidal <94443213+carlos54vidal@users.noreply.github.com>
2024-03-07 22:34:54 +0000

    update

--- payment/payment/urls.py
+++ payment/payment/urls.py
@@ -17,6 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# swagger
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', schema view)
 ]
