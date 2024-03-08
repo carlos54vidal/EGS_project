886cb7b950f190d0e9d4a87224ca62c43e7a261e
carlos54vidal <94443213+carlos54vidal@users.noreply.github.com>
2024-03-08 17:37:22 +0000

    update

--- payment/payment_api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CustomerDetail
-
-# Register your models here.
-
-admin.site.register(CustomerDetail)
--- payment/payment_api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import CustomerDetail
-
-class CustomerDetailSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomerDetail
-        fields = ['all']
