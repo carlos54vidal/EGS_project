a686b334e7513451ddb45fc98c19d6b43abcab73
carlos54vidal <94443213+carlos54vidal@users.noreply.github.com>
2024-03-20 17:36:06 +0000

    Update

--- payment-backend/payments/payment_api/serializers.py
+++ payment-backend/payments/payment_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Payments
 class PaymentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payments
-        fields = ('id', 'name', 'type', 'is_active', 'created_at', 'updated_at')
+        fields = ('id', 'client_unique_key', 'name', 'type', 'amount', 'paid', 'created_at', 'updated_at') 
