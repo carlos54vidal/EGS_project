f9957c3551104490553ea017717808b4be5f8ec1
carlos54vidal <94443213+carlos54vidal@users.noreply.github.com>
2024-04-16 01:47:42 +0100

    update

--- authentication-service/authentication/settings.py
+++ authentication-service/authentication/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'authentication.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/var/lib/sqlite/db.sqlite3',
     }
 }
 
