a630e9f1cf82dd1beed29b6cc5ad8a8a09bba59a
carlos54vidal <94443213+carlos54vidal@users.noreply.github.com>
2024-05-23 21:19:56 +0100

    Update settings.py

--- payment-backend/payments/payments/settings.py
+++ payment-backend/payments/payments/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'payment_api.middleware.middleware.PaymentsMiddleware',
+    #'payment_api.middleware.middleware.PaymentsMiddleware',
 ]
 
 ROOT_URLCONF = 'payments.urls'
